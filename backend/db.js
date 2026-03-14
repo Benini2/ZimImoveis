502ab0dd87b8e2ad07813546d1c1d3f161810a5e
Gabriel Benini <gabrielbenini77@gmail.com>
2026-03-14 13:40:00 -0300

    corrigir backend vercel

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,12 @@
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT), // Garante que é um número
-  ssl: {
-    rejectUnauthorized: false // Importante para conexões externas com Railway/Aiven
-  },
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  database: process.env.DB_NAME
 });
