0f7817242ef73ac6190a2dbbea3efbde9a3740f2
Gabriel Benini <gabrielbenini77@gmail.com>
2026-03-17 00:17:43 -0300

    conectar frontend com api railway

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,10 @@ export const pool = mysql.createPool({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 export default pool;
