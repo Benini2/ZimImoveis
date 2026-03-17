64fc8b265fdccb7c7fac2ffe14c0ae6c7aa13d8b
Gabriel Benini <gabrielbenini77@gmail.com>
2026-03-17 00:34:29 -0300

    conectar frontend com api railway

--- backend/db.js
+++ backend/db.js
@@ -4,6 +4,9 @@ const connectionURL = process.env.MYSQL_PUBLIC_URL;
 
 export const pool = mysql.createPool({
   uri: connectionURL,
+  ssl: {
+    rejectUnauthorized: true // obrigatório para proxies do Railway
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
