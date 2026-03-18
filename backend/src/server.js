22b7870b86cf531db9ebcbb1d2a674e09d276928
Gabriel Benini <gabrielbenini77@gmail.com>
2026-03-17 21:07:07 -0300

    Atualiza frontend e backend do cadastro de imóveis

--- backend/src/server.js
+++ backend/src/server.js
@@ -142,7 +142,7 @@ app.delete("/imoveis/:id", verificarToken, async (req, res) => {
 });
 
 // -------------------- SERVIDOR --------------------
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8800;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
